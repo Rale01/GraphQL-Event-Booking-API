2f5d3460d25c6e82fa1e7fbbaff39c2e0a6923ab
Rastko Jokic <rale01jokic@gmail.com>
2024-03-19 17:52:36 +0100

    Upoznavanje osnovnih koncepata GraphQL-a na jednostavnom primeru. Upoznavanje sa semama, jednostavnim resolverimai sa dva tipa operacija: query i mutation.

--- app.js
+++ app.js
@@ -7,9 +7,46 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World!');
-})
+const schema = buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
+
+const rootValue = {
+    events: () => {
+        return [
+            'Romanting Cooking',
+            'Sailing',
+            'All-Night Coding'
+        ];
+    },
+
+    createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+    }
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: rootValue,
+    graphiql: true // Enable GraphQL playground
+}));
+
+// Default route to open GraphQL playground without a query
+app.get('/', (req, res) => {
+    res.redirect('/graphql?query={}');
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
